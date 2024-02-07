c3dfeae11ed17d4823d2789dda3d9470d03fcf00
Anselmo <anselmo.cstecnologia@gmail.com>
2024-02-07 16:39:59 -0300

    alteracao data default

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-
+import datetime
 
 st.set_page_config(
         page_title="ANALISE AÇÕES - YFINANCE",
@@ -17,13 +17,22 @@ col1, col2 = st.columns(2)
 # Defina o dicionário de tickers fora do escopo do bloco
 tickers = {
     'ITAÚ - (ITUB4)': 'ITUB4.SA',
-    'PETROBRÁS -(PETR4)': 'PETR4.SA',
+    'PETROBRÁS - (PETR4)': 'PETR4.SA',
     'BRADESCO - (BBDC4)': 'BBDC4.SA',
-    'MAGALU - (MGLU3) ': 'MGLU3.SA',
+    'MAGALU - (MGLU3)': 'MGLU3.SA',
     'AMBEV - (ABEV3)': 'ABEV3.SA',
-    'VALE-(VALE3)': 'VALE3.SA',
-    'GERDAU - (GGBR4)': 'GGBR4.SA'
+    'VALE - (VALE3)': 'VALE3.SA',
+    'GERDAU - (GGBR4)': 'GGBR4.SA',
+    'VIA VAREJO - (VVAR3)': 'VVAR3.SA',
+    'BANCO DO BRASIL - (BBAS3)': 'BBAS3.SA',
+    'SANTANDER BRASIL - (SANB11)': 'SANB11.SA',
+    'BANRISUL - (BRSR6)': 'BRSR6.SA',
+    'BANCO ABC BRASIL - (ABCB4)': 'ABCB4.SA',
+    'BANESTES - (BEES3 e BEES4)': 'BEES3.SA',
+    'BANPARÁ - (BPAR3)': 'BPAR3.SA',
+    'BTG PACTUAL - (BPAC11)': 'BPAC11.SA'
 }
+
 
 
 ticker = st.sidebar.selectbox(
@@ -34,7 +43,7 @@ ticker_selecionado = tickers[ticker]
 
     
 with col1:
-    data_inicio = st.sidebar.date_input("Selecione a Data de Início", key="data_inicio")
+    data_inicio = st.sidebar.date_input("Selecione a Data de Início",datetime.date(2023, 1, 1), key="data_inicio")
     dt_inicio = pd.to_datetime(data_inicio).strftime('%Y-%m-%d')
     
 with col2:
@@ -47,20 +56,21 @@ historico_ticker = informacao_ticker.history(period='1d', start=dt_inicio, end=d
 st.write(" # :bar_chart: Análise de Ações com Yfinance ")
 
 st.divider()
-
+st.write('### Informações da Empresa')
 col1, col2, col3 = st.columns(3)
-
 col1.markdown(f"**Empresa:** {informacao_ticker.info['longName']}")
 col2.markdown(f"**Mercado :** {informacao_ticker.info['industryDisp']}")
 col3.markdown(f"**Preço Atual :** {informacao_ticker.info['currentPrice']} BRL")
 
 st.divider()
-
-st.write('Grafico de Preço de Fechamento')
-st.line_chart(historico_ticker['Close'])
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.write('### Grafico de Preço de Fechamento ')
+st.warning(f'Periodo :({dt_inicio} - {dt_final})')
+st.line_chart(historico_ticker['Close'], use_container_width=True)
    
-st.write('Gráfico de Variação Percentual Diário')
+st.write('### Gráfico de Variação Percentual Diário')
+st.warning(f'Periodo :({dt_inicio} - {dt_final})')
 historico_ticker['Variação Diária (%)'] = historico_ticker['Close'].pct_change() * 100
-st.line_chart(historico_ticker['Variação Diária (%)'], use_container_width=True)
+st.area_chart(historico_ticker['Variação Diária (%)'], use_container_width=True)
 
 st.caption('Os dados foram obtidos a partir do site : https://pypi.org/project/yfinance/')
